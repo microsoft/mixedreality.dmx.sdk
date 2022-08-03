3b16c386c64d451b403421e5ccf264e0c6bfd97e
Chaehong Lee <chaelee@microsoft.com>
2022-08-03 13:57:43 -0700

    models.artifact added

---
+++ DMX.Sdk/Models/Services/Foundations/Artifacts/Artifact.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+namespace DMX.Sdk.Models.Services.Foundations.Artifacts
+{
+    public class Artifact
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Size { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
