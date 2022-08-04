39451cda44f75c16d06d232536d158dc60f22abe
Geetika Jain <geetsk2194@gmail.com>
2022-08-04 10:28:44 -0700

    INFRA: Initialize Unit Test project

---
+++ DMX.Sdk.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DMX.Sdk.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
