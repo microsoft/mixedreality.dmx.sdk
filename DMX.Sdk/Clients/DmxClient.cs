17aebf02d3b78f05e49da0b78f4ac93a7dd01a67
Chaehong Lee <chaelee@microsoft.com>
2022-08-09 10:36:45 -0700

    CLIENT: added Dmx Client

---
+++ DMX.Sdk/Clients/DmxClient.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Sdk.Brokers.DmxApis;
+using DMX.Sdk.Brokers.Loggings;
+using DMX.Sdk.Models.Services.Foundations.Labs;
+using DMX.Sdk.Services.Foundations.Labs;
+using Microsoft.Extensions.Logging;
+
+namespace DMX.Sdk.Clients
+{
+    public class DmxClient
+    {
+        private readonly ILabService labService;
+
+        public DmxClient(
+            string environment,
+            string secret)
+        {
+            var dmxApiBroker = new DmxApiBroker(environment, secret);
+
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            var logger = loggerFactory.CreateLogger<LoggingBroker>();
+            var loggingBroker = new LoggingBroker(logger);
+
+            this.labService = new LabService(dmxApiBroker, loggingBroker);
+        }
+
+        public async ValueTask<List<Lab>> RetrieveAllLabsAsync() =>
+            await labService.RetrieveAllLabsAsync();
+
+
+    }
+}
---
+++ DMX.Sdk/Models/Clients/DmxEnvironments.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+
+namespace DMX.Sdk.Models.Clients
+{
+    public struct DmxEnvironments
+    {
+        public static string DEVELOPMENT = "https://dmx.dev";
+        public static string PRODUCTION = "https://dmx.dev";
+    }
+}
