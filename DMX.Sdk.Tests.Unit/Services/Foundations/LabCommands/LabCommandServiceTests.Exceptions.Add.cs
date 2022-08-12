1b48c0932e843dd6cccdef4dff50eb82bebf3d04
Chaehong Lee <chaelee@microsoft.com>
2022-08-12 10:52:37 -0700

    ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursLogItAsync -> FAIL

--- DMX.Sdk.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Exceptions.Add.cs
+++ DMX.Sdk.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Exceptions.Add.cs
@@ -5,10 +5,55 @@
 using Moq;
 using Xunit;
 using Xeptions;
+using DMX.Sdk.Models.LabCommands;
+using DMX.Sdk.Models.LabCommands.Exceptions;
+using DMX.Sdk.Models.Services.Foundation.Labs.Exceptions;
+using FluentAssertions;
 
 namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabCommands
 {
     public partial class LabCommandServiceTests
     {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyException))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursLogItAsync(
+            Xeption criticalDependencyException)
+        {
+            // given
+            LabCommand labCommand = CreateRandomLabCommand();
+
+            var failedLabCommandDependencyException =
+                new FailedLabCommandDependencyException(criticalDependencyException);
+
+            var expectedLabCommandDependencyException =
+                new LabCommandDependencyException(failedLabCommandDependencyException);
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.PostLabCommandAsync(It.IsAny<LabCommand>()))
+                    .ThrowsAsync(criticalDependencyException);
+
+            // when
+            ValueTask<LabCommand> returnedLabCommand =
+                this.labCommandService.AddLabCommandAsync(labCommand);
+
+            LabCommandDependencyException actualLabCommandDependencyException =
+                await Assert.ThrowsAsync<LabCommandDependencyException>(returnedLabCommand.AsTask);
+
+            // then
+            actualLabCommandDependencyException
+                .Should().BeEquivalentTo(expectedLabCommandDependencyException);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.PostLabCommandAsync(labCommand),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(
+                    It.Is(SameExceptionAs(expectedLabCommandDependencyException))),
+                        Times.Once);
+
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ DMX.Sdk/Models/LabCommands/Exceptions/FailedLabCommandDependencyException.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace DMX.Sdk.Models.LabCommands.Exceptions
+{
+    public class FailedLabCommandDependencyException : Xeption
+    {
+        public FailedLabCommandDependencyException(Xeption innerException)
+            : base(message: "Failed lab command dependency exception occured. Please contact support.", innerException)
+        { }
+    }
+}
