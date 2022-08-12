8dc6af5fb17800808b73b727eabc2e0515075575
Chaehong Lee <chaelee@microsoft.com>
2022-08-12 13:41:11 -0700

    CLIENT: Send lab command

--- DMX.Sdk/Clients/DmxClient.cs
+++ DMX.Sdk/Clients/DmxClient.cs
@@ -4,7 +4,9 @@
 
 using DMX.Sdk.Brokers.DmxApis;
 using DMX.Sdk.Brokers.Loggings;
+using DMX.Sdk.Models.LabCommands;
 using DMX.Sdk.Models.Services.Foundations.Labs;
+using DMX.Sdk.Services.Foundations.LabCommands;
 using DMX.Sdk.Services.Foundations.Labs;
 using Microsoft.Extensions.Logging;
 
@@ -13,6 +15,7 @@ namespace DMX.Sdk.Clients
     public class DmxClient
     {
         private readonly ILabService labService;
+        private readonly ILabCommandService labCommandService;
 
         public DmxClient(
             string environment,
@@ -25,9 +28,13 @@ namespace DMX.Sdk.Clients
             var loggingBroker = new LoggingBroker(logger);
 
             this.labService = new LabService(dmxApiBroker, loggingBroker);
+            this.labCommandService = new LabCommandService(dmxApiBroker, loggingBroker);
         }
 
         public async ValueTask<List<Lab>> RetrieveAllLabsAsync() =>
             await labService.RetrieveAllLabsAsync();
+
+        public async ValueTask<LabCommand> SendLabCommandAsync(LabCommand labCommand) =>
+            await labCommandService.AddLabCommandAsync(labCommand);
     }
 }
