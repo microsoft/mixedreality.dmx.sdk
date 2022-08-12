b1b05350ef6c6bfc3efcecc92ccf0f71ecf268b1
Terry Palomares <tpalomares18@gmail.com>
2022-08-12 11:00:26 -0700

    ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursLogItAsync -> PASS

--- DMX.Sdk/Services/Foundations/LabCommands/LabCommandService.Exceptions.cs
+++ DMX.Sdk/Services/Foundations/LabCommands/LabCommandService.Exceptions.cs
@@ -6,6 +6,7 @@ using DMX.Sdk.Brokers.DmxApis;
 using DMX.Sdk.Brokers.Loggings;
 using DMX.Sdk.Models.LabCommands;
 using DMX.Sdk.Models.LabCommands.Exceptions;
+using RESTFulSense.Exceptions;
 using Xeptions;
 
 namespace DMX.Sdk.Services.Foundations.LabCommands
@@ -19,16 +20,46 @@ namespace DMX.Sdk.Services.Foundations.LabCommands
             {
                 return await returningLabCommandFunction();
             }
+            catch (HttpResponseUrlNotFoundException httpResponseUrlNotFoundException)
+            {
+                var failedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(httpResponseUrlNotFoundException);
+
+                throw CreateAndLogDependencyException(failedLabCommandDependencyException);
+            }
+            catch (HttpResponseUnauthorizedException httpResponseUnauthorizedException)
+            {
+                var failedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(httpResponseUnauthorizedException);
+
+                throw CreateAndLogDependencyException(failedLabCommandDependencyException);
+            }
+            catch (HttpResponseForbiddenException httpResponseForbiddenException)
+            {
+                var failedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(httpResponseForbiddenException);
+
+                throw CreateAndLogDependencyException(failedLabCommandDependencyException);
+            }
             catch (NullLabCommandException nullLabCommandException)
             {
                 throw CreateAndLogValidationException(nullLabCommandException);
             }
         }
 
-        private LabCommandValidationException CreateAndLogValidationException(Xeption xeption)
+        private Exception CreateAndLogDependencyException(Xeption exception)
         {
-            var labCommandValidationException = new LabCommandValidationException(xeption);
+            var labCommandDependencyException = new LabCommandDependencyException(exception);
+            this.loggingBroker.LogCritical(labCommandDependencyException);
+
+            return labCommandDependencyException;
+        }
+
+        private LabCommandValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var labCommandValidationException = new LabCommandValidationException(exception);
             this.loggingBroker.LogError(labCommandValidationException);
+
             return labCommandValidationException;
 
         }
