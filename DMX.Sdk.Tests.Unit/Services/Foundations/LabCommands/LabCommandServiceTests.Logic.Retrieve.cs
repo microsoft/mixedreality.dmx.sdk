afd9cb8545677a389bf1b911ff1b854310c2ef8f
Chaehong Lee <chaelee@microsoft.com>
2022-08-16 10:43:42 -0700

    ShouldRetrieveLabCommandAsync -> FAIL

---
+++ DMX.Sdk.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Logic.Retrieve.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Moq;
+using Xunit;
+using Xeptions;
+using DMX.Sdk.Models.LabCommands;
+using FluentAssertions;
+using Force.DeepCloner;
+
+namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabCommands
+{
+    public partial class LabCommandServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveLabCommandAsync()
+        {
+            // given
+            LabCommand randomLabCommand = CreateRandomLabCommand();
+            Guid inputLabCommandId = randomLabCommand.Id;
+            LabCommand postedLabCommand = randomLabCommand;
+            LabCommand expectedLabCommand = randomLabCommand.DeepClone();
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.GetLabCommandByIdAsync(inputLabCommandId))
+                    .ReturnsAsync(postedLabCommand);
+
+            // when
+            LabCommand actualLabCommand =
+                await this.labCommandService.RetrieveLabCommandByIdAsync(
+                    inputLabCommandId);
+
+            // then
+            actualLabCommand.Should().BeEquivalentTo(expectedLabCommand);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.GetLabCommandByIdAsync(inputLabCommandId),
+                    Times.Once);
+
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
