e6ec69bafb8939562d8206243d3bb6cc880d7be4
Chaehong Lee <chaelee@microsoft.com>
2022-08-16 10:56:48 -0700

    ShouldThrowCriticalDependencyExceptionOnRetrieveIfCriticalErrorOccursLogItAsync -> FAIL

---
+++ DMX.Sdk.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Exceptions.Retrieve.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Moq;
+using Xunit;
+using Xeptions;
+using DMX.Sdk.Models.LabCommands;
+using DMX.Sdk.Models.LabCommands.Exceptions;
+using FluentAssertions;
+using RESTFulSense.Exceptions;
+
+namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabCommands
+{
+    public partial class LabCommandServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyException))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveIfCriticalErrorOccursLogItAsync(
+            Xeption criticalDependencyException)
+        {
+            // given
+            LabCommand labCommand = CreateRandomLabCommand();
+            Guid labCommandId = labCommand.Id;
+
+            var failedLabCommandDependencyException =
+                new FailedLabCommandDependencyException(criticalDependencyException);
+
+            var expectedLabCommandDependencyException =
+                new LabCommandDependencyException(failedLabCommandDependencyException);
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.GetLabCommandByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(criticalDependencyException);
+
+            // when
+            ValueTask<LabCommand> returnedLabCommand =
+                this.labCommandService.RetrieveLabCommandByIdAsync(labCommandId);
+
+            LabCommandDependencyException actualLabCommandDependencyException =
+                await Assert.ThrowsAsync<LabCommandDependencyException>(
+                    returnedLabCommand.AsTask);
+
+            // then
+            actualLabCommandDependencyException
+                .Should().BeEquivalentTo(expectedLabCommandDependencyException);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.GetLabCommandByIdAsync(labCommandId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(
+                    It.Is(SameExceptionAs(expectedLabCommandDependencyException))),
+                        Times.Once);
+
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
