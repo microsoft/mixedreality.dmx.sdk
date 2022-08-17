cbb8be1cb1964f87399ca0062db91b347a497305
Geetika Jain <geetsk2194@gmail.com>
2022-08-17 10:30:01 -0700

    ShouldThrowValidationExceptionIfLabCommandIdIsInvalidAndLogItAsync -> FAIL

---
+++ DMX.Sdk.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Validation.Retrieve.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Sdk.Models.LabCommands;
+using DMX.Sdk.Models.LabCommands.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabCommands
+{
+    public partial class LabCommandServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionIfLabCommandIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidLabCommandId = Guid.Empty;
+            var nullLabCommandIdException = new NullLabCommandIdException();
+
+            var expectedLabCommandValidationException =
+                new LabCommandValidationException(nullLabCommandIdException);
+
+            // when
+            ValueTask<LabCommand> retrieveLabCommandByIdTask =
+                this.labCommandService.RetrieveLabCommandByIdAsync(invalidLabCommandId);
+
+            LabCommandValidationException actualLabCommandValidationException =
+                await Assert.ThrowsAsync<LabCommandValidationException>(
+                    retrieveLabCommandByIdTask.AsTask);
+
+            // then
+            actualLabCommandValidationException.Should().BeEquivalentTo(
+                expectedLabCommandValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLabCommandValidationException))),
+                        Times.Once);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.GetLabCommandByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ DMX.Sdk/Models/LabCommands/Exceptions/NullLabCommandIdException.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Sdk.Models.LabCommands.Exceptions
+{
+    public partial class NullLabCommandIdException : Xeption
+    {
+        public NullLabCommandIdException()
+            : base(message: "Lab command id is null.")
+        {
+        }
+    }
+}
