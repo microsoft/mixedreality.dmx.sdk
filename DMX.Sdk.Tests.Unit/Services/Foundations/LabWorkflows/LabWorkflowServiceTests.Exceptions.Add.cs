bcf5b0b6b2a6452b956eaf79c30401b2356746b2
Geetika Jain <geetsk2194@gmail.com>
2022-10-10 16:12:07 -0700

    ShouldThrowDependencyExceptionOnAddIfErrorOccursAndLogItAsync -> FAIL

--- DMX.Sdk.Tests.Unit/Services/Foundations/LabWorkflows/LabWorkflowServiceTests.Exceptions.Add.cs
+++ DMX.Sdk.Tests.Unit/Services/Foundations/LabWorkflows/LabWorkflowServiceTests.Exceptions.Add.cs
@@ -8,6 +8,9 @@ using Xunit;
 using DMX.Sdk.Models.LabWorkflows;
 using DMX.Sdk.Models.LabWorkflows.Exceptions;
 using FluentAssertions;
+using DMX.Sdk.Models.LabWorkflows.Exceptions;
+using DMX.Sdk.Models.LabWorkflows;
+using RESTFulSense.Exceptions;
 
 namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabWorkflows
 {
@@ -15,7 +18,7 @@ namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabWorkflows
     {
         [Theory]
         [MemberData(nameof(CriticalDependencyException))]
-        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursLogItAsync(
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursLogItAsync (
             Xeption criticalDependencyException)
         {
             // given
@@ -50,6 +53,49 @@ namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabWorkflows
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(
                     It.Is(SameExceptionAs(expectedLabWorkflowDependencyException))),
+                        Times.Once);
+
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfErrorOccursAndLogItAsync()
+        {
+            // given
+            LabWorkflow someLabWorkflow = CreateRandomLabWorkflow();
+
+            var httpResponseException = new HttpResponseException();
+
+            var failedLabWorkflowDependencyException
+                = new FailedLabWorkflowDependencyException(httpResponseException);
+
+            var expectedLabWorkflowDependencyException =
+                new LabWorkflowDependencyException(failedLabWorkflowDependencyException);
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.PostLabWorkflowAsync(It.IsAny<LabWorkflow>()))
+                    .ThrowsAsync(httpResponseException);
+
+            // when
+            ValueTask<LabWorkflow> addLabWorkflowTask =
+                this.labWorkflowService.AddLabWorkflowAsync(someLabWorkflow);
+
+            LabWorkflowDependencyException actualLabWorkflowDependencyException =
+                await Assert.ThrowsAsync<LabWorkflowDependencyException>(
+                    addLabWorkflowTask.AsTask);
+
+            // then
+            actualLabWorkflowDependencyException.Should()
+                .BeEquivalentTo(expectedLabWorkflowDependencyException);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.PostLabWorkflowAsync(It.IsAny<LabWorkflow>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLabWorkflowDependencyException))),
                         Times.Once);
 
             this.dmxApiBrokerMock.VerifyNoOtherCalls();
