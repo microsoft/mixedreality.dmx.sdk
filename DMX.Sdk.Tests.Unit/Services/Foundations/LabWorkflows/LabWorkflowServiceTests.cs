ae05b5f09859667a37710555f3964e05043babcb
Geetika Jain <geetsk2194@gmail.com>
2022-10-10 14:58:50 -0700

    ShouldAddLabWorkflowAsync -> FAIL

---
+++ DMX.Sdk.Tests.Unit/Services/Foundations/LabWorkflows/LabWorkflowServiceTests.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using DMX.Sdk.Brokers.DmxApis;
+using DMX.Sdk.Brokers.Loggings;
+using DMX.Sdk.Models.LabWorkflows;
+using DMX.Sdk.Services.Foundations.LabWorkflows;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace DMX.Sdk.Tests.Unit.Services.Foundations.LabWorkflows
+{
+    public partial class LabWorkflowServiceTests
+    {
+        private readonly Mock<IDmxApiBroker> dmxApiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly LabWorkflowService labWorkflowService;
+
+        public LabWorkflowServiceTests()
+        {
+            this.dmxApiBrokerMock = new Mock<IDmxApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.labWorkflowService = new LabWorkflowService(
+                dmxApiBroker: this.dmxApiBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private Filler<LabWorkflow> CreateRandomLabWorkflowFiller()
+        {
+            var filler = new Filler<LabWorkflow>();
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset);
+
+            return filler;
+        }
+
+        private LabWorkflow CreateRandomLabWorkflow() =>
+            CreateRandomLabWorkflowFiller().Create();
+    }
+}
---
+++ DMX.Sdk/Services/Foundations/LabWorkflows/LabWorkflowService.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Sdk.Brokers.DmxApis;
+using DMX.Sdk.Brokers.Loggings;
+using DMX.Sdk.Models.LabWorkflows;
+
+namespace DMX.Sdk.Services.Foundations.LabWorkflows
+{
+    public partial class LabWorkflowService
+    {
+        private readonly IDmxApiBroker dmxApiBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public LabWorkflowService(
+            IDmxApiBroker dmxApiBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.dmxApiBroker = dmxApiBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<LabWorkflow> AddLabWorkflowAsync(LabWorkflow labWorkflow) =>
+            throw new NotImplementedException();
+    }
+}
